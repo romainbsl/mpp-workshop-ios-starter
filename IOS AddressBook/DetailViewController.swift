09b89f3591be0adf771547d473c89b7b9f952d5d
Romain Boisselle <romain@boisselle.dev>
2019-12-02 12:20:32 +0100

    addressbook-common library usage

--- IOS AddressBook/DetailViewController.swift
+++ IOS AddressBook/DetailViewController.swift
@@ -1,23 +1,50 @@
 import UIKit
+import AddressBookCommon
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController ,ContactDetailView {
     
+    private var presenter: ContactDetailPresenter!
     var contactId: String?
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var birthfayLabel: UILabel!
     @IBOutlet weak var phonesLabel: UILabel!
     @IBOutlet weak var addressesLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter = CommonInjector.init().contactDetailPresenter()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        presenter.attachView(view: self)
+        presenter.getContact(contactId: contactId!)    
     }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        presenter.detachView()
     }
+    
+    func displayContact(contact: Contact) {
+           nameLabel.text = contact.fullName
+
+           for (index,phone) in contact.phones.enumerated() {
+               phonesLabel.text! += phone.type.displayedName + ": "
+                                   + phone.number
+
+               if index < contact.phones.count-1 { phonesLabel.text! += "\n" }
+           }
+
+           for (index, address) in contact.addresses.enumerated() {
+               addressesLabel.text! += address.type.displayedName + ":\n "
+                               + address.street + "\n"
+                               + address.postalCode + " " + address.city + "\n"
+                               + address.country
+
+               if index < contact.addresses.count - 1 {
+                   addressesLabel.text! += "\n------------------------------\n"
+               }
+           }
+       }
 }
--- IOS AddressBook/MasterViewController.swift
+++ IOS AddressBook/MasterViewController.swift
@@ -1,37 +1,51 @@
 import UIKit
+import AddressBookCommon
 
-class MasterViewController: UITableViewController {
+class MasterViewController: UITableViewController , ContactListView{
     
-    private var contactList = [Any]()
+    private var contactList = [Contact]()
+    private var presenter: ContactListPresenter!
     var detailViewController: DetailViewController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter = CommonInjector.init().contactListPresenter()
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+       super.viewWillAppear(animated)
+       presenter.attachView(view: self)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+       super.viewWillDisappear(animated)
+       presenter.detachView()
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    func displayContactList(contactList: [Contact]) {
+       self.contactList = contactList
+       self.tableView.reloadData()
     }
     
     // MARK: - Segues
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
-            // TODO - Configure segue
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let contact = contactList[indexPath.row]
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.contactId = contact.id
+            }
         }
     }
     
     // MARK: - Table View
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0 // TODO - return the contact list size
+           return contactList.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        // TODO - Set what to display in the Table View for each contact
-        return cell
+       let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+       let contact = contactList[indexPath.row]
+       cell.textLabel!.text = contact.fullName
+       return cell
     }
 }
 
